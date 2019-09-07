d770601e59f93ed2c557157bec08581526c70794
hjälte <hjalte79@gmail.com>
2019-09-07 17:58:38 +0200

    Added Copyright info

--- ShowFullyConstraint/StandardAddInServer.cs
+++ ShowFullyConstraint/StandardAddInServer.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) Hjälte. All rights reserved 
+// Written by Jelte de Jong - 2019
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// HJALTE PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// HJALTE SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
